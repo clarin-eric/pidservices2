e62715d62c8c40e2ed8bd1df589740bafd42d926
Thomas Eckart <teckart@informatik.uni-leipzig.de>
2014-03-07 15:22:20 +0100

    Override toString()

--- src/main/java/de/uni_leipzig/asv/clarin/webservices/pidservices2/PidObject.java
+++ src/main/java/de/uni_leipzig/asv/clarin/webservices/pidservices2/PidObject.java
@@ -1,5 +1,7 @@
 package de.uni_leipzig.asv.clarin.webservices.pidservices2;
 
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 import net.sf.json.JSONArray;
@@ -8,18 +10,19 @@ import com.jayway.jsonpath.JsonPath;
 import com.jayway.jsonpath.PathNotFoundException;
 
 /**
- * Stores most relevant information of a PID JSON object retrieved from the GWDG
+ * Stores most(!) relevant information of a PID JSON object retrieved from the GWDG
  * 
  * @author Thomas Eckart
  * 
  */
 public class PidObject {
 	private final String handleIdentifier;
-	private Map<HandleField, String> fieldMap;
+	private final Map<HandleField, String> fieldMap;
 
 	public PidObject(String pid, JSONArray pidJsonArray) {
 		this.handleIdentifier = pid;
 
+		fieldMap = new HashMap<HandleField, String>();
 		String jsonPath;
 		for (HandleField fieldName : HandleField.values()) {
 			try {
@@ -49,5 +52,18 @@ public class PidObject {
 	 */
 	public String getValue(HandleField fieldName) {
 		return fieldMap.get(fieldName);
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder(handleIdentifier);
+		sb.append(" (Values: ");
+		Iterator<HandleField> fieldIterator = fieldMap.keySet().iterator();
+		while (fieldIterator.hasNext()) {
+			HandleField field = fieldIterator.next();
+			sb.append(" " + field + "=\"" + fieldMap.get(field) + "\"");
+		}
+		sb.append(")");
+		return sb.toString();
 	}
 }
