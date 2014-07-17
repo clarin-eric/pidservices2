f268212e2007653397c7dbedfee7e76ff0dff22b
Thomas Eckart <teckart@informatik.uni-leipzig.de>
2014-07-17 10:07:39 +0200

    Javadoc

--- src/main/java/de/uni_leipzig/asv/clarin/webservices/pidservices2/HandleField.java
+++ src/main/java/de/uni_leipzig/asv/clarin/webservices/pidservices2/HandleField.java
@@ -1,7 +1,7 @@
 package de.uni_leipzig.asv.clarin.webservices.pidservices2;
 
 /**
- * Fields that can be stored via the EPIC v2 API. This enumeration is a restriction of allowed fields in this project. The EPIC API does not have such
+ * Fields that can be stored via the EPIC v2 API. This enumeration is a restriction of allowed fields <em>in this project</em>. The EPIC API does not have such
  * restrictions.
  * 
  * @author Thomas Eckart
